e833100d5a65074e25ef3059c47a2694231802ce
Taylor Page <jonathantaylorpage@gmail.com>
2018-01-13 17:31:53 -0600

    Create address submission test

--- src/Platform/Platform.js
+++ src/Platform/Platform.js
@@ -2,8 +2,43 @@ import React    from "react";
 import template from "./Platform.jsx";
 
 class Platform extends React.Component {
+
+  constructor(props) {
+    super();
+
+    this.state = {
+      address: ''
+    }
+
+    // Set to lisk test net
+    this.setTestnet();
+  }
+  
+  lisk = window.lisk;
+
+  // Sets lisk to test environment
+  setTestnet() {
+    this.lisk.api().setTestnet(true);
+  }
+
+  // Sets the state when form changes
+  detectFormChanges(elem, prop) {
+    const state = {};
+    state[prop] = elem.target.value;
+    this.setState(state);
+  }
+
+  // test subbmit functionality
+  test() {
+    console.log(this.state.address);
+  }
+
   render() {
-    return template.call(this);
+    return template.call(
+      this,
+      this.detectFormChanges,
+      this.test
+    );
   }
 }
 
--- src/Platform/Platform.jsx
+++ src/Platform/Platform.jsx
@@ -1,10 +1,20 @@
 import "./Platform.css";
 import React from "react";
 
-function template() {
+function template(
+  changeDetector,
+  test
+) {
   return (
     <div className="platform">
       <h1>Platform</h1>
+      <input
+        type="text"
+        onChange={
+          (elem) => changeDetector.bind(this, elem, 'address')()
+        }>
+      </input>
+      <button onClick={test.bind(this)}>Find Account</button>
     </div>
   );
 };
