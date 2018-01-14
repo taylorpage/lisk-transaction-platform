c6ff5bdf3c13999dc8b02347bbfba854f47ea6af
Taylor Page <jonathantaylorpage@gmail.com>
2018-01-13 19:01:51 -0600

    Allow a passphrase to be converted to a secret key and fetch account keys

--- src/Platform/Platform.js
+++ src/Platform/Platform.js
@@ -1,13 +1,19 @@
 import React    from "react";
 import template from "./Platform.jsx";
 
+import bip39 from 'bip39';
+
 class Platform extends React.Component {
 
   constructor(props) {
     super();
 
     this.state = {
-      address: ''
+      passPhrase: '',
+
+      // Keys
+      publicKey: '',
+      privateKey: ''
     }
 
     // Set to lisk test net
@@ -15,10 +21,16 @@ class Platform extends React.Component {
   }
   
   lisk = window.lisk;
+  bip39 = bip39;
 
   // Sets lisk to test environment
   setTestnet() {
     this.lisk.api().setTestnet(true);
+  }
+
+  // Generates a seed hex from a passphrase
+  generateSeedHex(mnemonic) {
+    return bip39.mnemonicToSeedHex(mnemonic);
   }
 
   // Sets the state when form changes
@@ -29,15 +41,24 @@ class Platform extends React.Component {
   }
 
   // test subbmit functionality
-  test() {
-    console.log(this.state.address);
+  getKeysFromSecret() {
+    const seedHex = this.generateSeedHex(this.state.address);
+    const keys = this.lisk.crypto.getKeys(seedHex);
+
+    // Set keys on the state
+    this.setState({
+      privateKey: keys.privateKey,
+      publicKey: keys.publicKey
+    });
   }
 
   render() {
     return template.call(
       this,
       this.detectFormChanges,
-      this.test
+      this.getKeysFromSecret,
+      this.state.publicKey,
+      this.state.privateKey
     );
   }
 }
--- src/Platform/Platform.jsx
+++ src/Platform/Platform.jsx
@@ -3,18 +3,28 @@ import React from "react";
 
 function template(
   changeDetector,
-  test
+  getKeysFromSecret,
+  publicKey,
+  privateKey
 ) {
   return (
     <div className="platform">
       <h1>Platform</h1>
+      <h5>Get keys from your secret lisk pass phrase</h5>
       <input
         type="text"
         onChange={
-          (elem) => changeDetector.bind(this, elem, 'address')()
+          (elem) => changeDetector.bind(this, elem, 'passPhrase')()
         }>
       </input>
-      <button onClick={test.bind(this)}>Find Account</button>
+      <button onClick={ getKeysFromSecret.bind(this) }>Find Account</button>
+
+      <h4>Public Key:</h4>
+      <h6>{ publicKey }</h6>
+
+      <h4>Private Key:</h4>
+      <h6>{ privateKey }</h6>
+
     </div>
   );
 };
